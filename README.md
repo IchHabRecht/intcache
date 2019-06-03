961b631fd7f0353496fd8246ee70ce7640a12320
Nicole Cordes <typo3@cordes.co>
2019-06-03 18:49:57 +0200

    [TASK] Add basic extension files
    Add supporting files from EXT:my_skeleton 0.2.0

---
+++ README.md
@@ -0,0 +1,19 @@
+# TYPO3 Extension intcache
+
+[![Latest Stable Version](https://img.shields.io/packagist/v/ichhabrecht/intcache.svg)](https://packagist.org/packages/ichhabrecht/intcache)
+[![Build Status](https://img.shields.io/travis/IchHabRecht/intcache/master.svg)](https://travis-ci.org/IchHabRecht/intcache)
+[![StyleCI](https://github.styleci.io/repos/190002031/shield?branch=master)](https://github.styleci.io/repos/190002031)
+
+Turn uncachable page objects into cacheable links
+
+## Features
+
+## Installation
+
+Simply install the extension with Composer or the [Extension Manager](https://extensions.typo3.org/extension/intcache/).
+
+`composer require ichhabrecht/intcache`
+
+## Usage
+
+## Community
---
+++ ext_emconf.php
@@ -0,0 +1,40 @@
+<?php
+
+/***************************************************************
+ * Extension Manager/Repository config file for ext "intcache".
+ *
+ * Auto generated 03-06-2019 15:16
+ *
+ * Manual updates:
+ * Only the data in the array - everything else is removed by next
+ * writing. "version" and "dependencies" must not be touched!
+ ***************************************************************/
+
+$EM_CONF[$_EXTKEY] = array (
+  'title' => 'intcache',
+  'description' => 'Turn uncachable page objects into cacheable links',
+  'category' => 'misc',
+  'author' => 'Nicole Cordes',
+  'author_email' => 'typo3@cordes.co',
+  'author_company' => 'biz-design',
+  'state' => 'stable',
+  'uploadfolder' => 0,
+  'createDirs' => '',
+  'clearCacheOnLoad' => 0,
+  'version' => '0.1.0',
+  'constraints' => 
+  array (
+    'depends' => 
+    array (
+      'typo3' => '7.6.0-9.5.99',
+    ),
+    'conflicts' => 
+    array (
+    ),
+    'suggests' => 
+    array (
+    ),
+  ),
+  '_md5_values_when_last_written' => 'a:7:{s:7:"LICENSE";s:4:"b234";s:9:"README.md";s:4:"cd36";s:13:"composer.json";s:4:"c2a7";s:12:"ext_icon.png";s:4:"2f84";s:16:"phpunit.xml.dist";s:4:"6068";s:24:"sonar-project.properties";s:4:"b13b";s:36:"Resources/Public/Icons/Extension.svg";s:4:"8ec3";}',
+);
+
