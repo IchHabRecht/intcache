774e98ec8c133d2a5b6ea2076a70f08c275fadf7
Nicole Cordes <typo3@cordes.co>
2019-06-05 20:49:14 +0200

    [FEATURE] Add TypoScript and Fluid templates

---
+++ Configuration/TCA/Overrides/tt_content.php
@@ -0,0 +1,10 @@
+<?php
+defined('TYPO3_MODE') || die();
+
+call_user_func(function () {
+    \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile(
+        'intcache',
+        'Configuration/TypoScript/',
+        'intcache'
+    );
+});
